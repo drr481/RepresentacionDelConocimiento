d697801cf61b9324792c0ca2f60933e16ab7c482
drr481 <drr481@alumnos.unican.es>
2024-10-11 16:23:47 +0200

    dependencias(factor) arreglado

--- Practica2/Main.py
+++ Practica2/Main.py
@@ -1,6 +1,8 @@
 # Input: Conjunto de Factores
 # Output: 
+import Factor as f
 
-str = "e^0"
-print(str.split('^'))
-print(str.split('^')[0])
+f_A = f.Factor('A', ['d^0', 'd^1', 'e^0', 'e^1'], [[0.3, 0.4, 0.3], [0.1, 0.2, 0.7], [0.7, 0.2, 0.1], [0.2, 0.3, 0.5]], propios=['a^0', 'a^1', 'a^2'])
+
+f_A.imprimir()
+print(f.dependencias(f_A))
